d15e513c39766eabf0d291502976af444ea808ec
Mobiuss <jf1033224672@gamil.com>
2025-04-07 20:47:24 +0800

    独立input框+初始页面+发音

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,104 @@
 <template>
     <div class="app">
-        <header class="translation">{{ current_word.translation }}</header>
-        <div class="inputs_container">
-
+        <header>{{ current_word.translation }}</header>
+        <div ref="inputs_container_ref" class="inputs_container">
+            <input v-for="(_, index) in current_word.content" v-bind:key="current_word.id.toString() + index.toString()"
+                type="text" @input="(e) => handleTyping(e, index)">
         </div>
-        <input v-model="input_content" ref="input" type="text" @input="handleType" />
+        <!-- <button @click="playAudio">播放</button> -->
+        <dialog ref="ready_dialog_ref">
+            <span @click="close_dialog">
+                Are you ready? Press the Enter to begin.
+            </span>
+        </dialog>
     </div>
 </template>
 
 <script setup lang="ts">
 import { words } from "@/data.ts";
-import { computed, onMounted, ref, type Ref } from "vue";
+import { computed, nextTick, onMounted, ref, type Ref } from "vue";
 
 const count = ref(0)
-const index = ref(0)
-const input_content = ref("")
-const input: Ref<HTMLInputElement | null> = ref(null)
+const inputs_container_ref: Ref<HTMLDivElement | null> = ref(null)
 const current_word = computed(() => {
     return words[count.value]
 })
+const ready_dialog_ref: Ref<HTMLDialogElement | null> = ref(null)
 
-const handleType = () => {
-    index.value = (input_content.value.length - 1 === -1) ? 0 : input_content.value.length - 1
-    if (current_word.value.content[index.value] === input_content.value[index.value]) {
-        if (index.value === current_word.value.content.length - 1) {
-            setTimeout(() => {
-                move_next()
-            }, 300);
-        }
-    } else {
-        setTimeout(() => {
-            input_content.value = input_content.value.substring(0, input_content.value.length - 1)
-        }, 300);
-    }
+const playAudio = () => {
+    new Audio(current_word.value.voiceUrl).play()
 }
 
-const move_next = () => {
+const handleTyping = (event: Event, index: number) => {
+    const current_input_element = (event.target as HTMLInputElement)
+    const current_input_value = current_input_element.value
+    const wanted_value = current_word.value.content[index]
+    if (current_input_value === wanted_value) {
+        if (index === current_word.value.content.length - 1) {
+            current_input_element.setAttribute("class", "success")
+            setTimeout(() => {
+                move_next_word()
+            }, 300);
+        } else {
+            current_input_element.setAttribute("class", "success")
+            current_input_element.setAttribute("disabled", "true")
+            focus_input(index + 1)
+        }
+    } else {
+        current_input_element.setAttribute("class", "failed")
+        setTimeout(() => {
+            current_input_element.value = ""
+        }, 300);
+    }
+
+}
+
+const move_next_word = () => {
+    if (count.value === words.length - 1) {
+        alert("背完啦")
+        count.value = 0
+        nextTick(() => {
+            focus_input(0)
+        })
+        return
+    }
     count.value += 1
-    index.value = 0
-    input_content.value = ""
+    nextTick(() => {
+        focus_input(0)
+        playAudio()
+    })
+}
+
+const focus_input = (index: number) => {
+    const index_input = inputs_container_ref.value?.children[index] as HTMLInputElement
+    index_input.focus()
+}
+
+
+const close_dialog = () => {
+    ready_dialog_ref.value?.close()
+}
+
+const bindKeys = () => {
+    window.addEventListener('keydown', (event) => {
+        switch (event.key) {
+            case "Enter":
+                close_dialog()
+                setTimeout(() => {
+                    focus_input(0)
+                    playAudio()
+                }, 300);
+                break;
+            default:
+                break;
+        }
+    });
 }
 
 onMounted(() => {
-    input.value?.focus()
+    focus_input(0)
+    ready_dialog_ref.value?.show()
+    bindKeys()
 })
 </script>
 
@@ -53,22 +109,61 @@ onMounted(() => {
     align-items: center;
     justify-content: center;
     gap: 30px;
-    width: 80%;
+    width: 100%;
     margin: 0 auto;
-    margin-top: 300px;
+    margin-top: 20%;
 }
 
 .app header {
-    text-align: center;
     font-size: large;
 }
 
-.app input {
-    width: 30%;
+.app .inputs_container {
+    width: 100%;
     height: 50px;
+
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    gap: 20px;
+}
+
+.app .inputs_container input {
+    box-sizing: border-box;
+    border: 0px;
+    border-bottom: 1px black solid;
+    outline: none;
+    padding: 10px;
+    background-color: white;
+    width: 50px;
+
     font-size: larger;
-
     letter-spacing: 10px;
+    text-align: center;
 
+}
+
+.app .inputs_container input.success {
+    color: blue;
+    border: 0px;
+}
+
+.app .inputs_container input.failed {
+    color: red;
+}
+
+dialog {
+    padding: 0;
+    margin: 0;
+    border: 0;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    box-sizing: border-box;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    line-height: 100vh;
 }
 </style>
--- src/data.ts
+++ src/data.ts
@@ -2,13 +2,26 @@ interface word {
   id: number
   content: string
   translation: string
-  voice?: string
+  pronunciation?: string
+  voiceUrl?: string
 }
 
 export const words: word[] = [
-  { id: 1, content: 'hello', translation: '你好' },
-  { id: 2, content: 'world', translation: '世界' },
-  { id: 3, content: 'practice', translation: '联系' },
+  {
+    id: 1,
+    content: 'hello',
+    translation: '你好',
+    voiceUrl:
+      'https://dictionary.cambridge.org/media/english-chinese-traditional/us_pron/h/hel/hello/hello.mp3',
+  },
+  {
+    id: 2,
+    content: 'world',
+    translation: '世界',
+    voiceUrl:
+      'https://dictionary.cambridge.org/media/english-chinese-traditional/us_pron/w/wor/world/world.mp3',
+  },
+  { id: 3, content: 'practice', translation: '练习' },
   { id: 4, content: 'make', translation: '做' },
   { id: 5, content: 'perfect', translation: '完美' },
 ]
